0b5dda7f87a794a2bf95696359beaad6a5ccecab
Cesar Caselles <ccaselles815@unab.edu.co>
2026-02-27 09:42:05 -0500

    Pantalla 2.1

--- QuizAndroidAndresCala/app/src/main/java/me/cesarcaselles/quizandroidandrescala/MainActivity.kt
+++ QuizAndroidAndresCala/app/src/main/java/me/cesarcaselles/quizandroidandrescala/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.cesarcaselles.quizandroidandrescala.ui.theme.QuizAndroidAndresCalaTheme
@@ -95,5 +97,10 @@ fun segundaPantalla(){
         verticalArrangement = Arrangement.SpaceBetween){
         val painter = painterResource(R.drawable.user)
 
+        Text(
+            text = "aaaaaaa",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.size(35.dp)
+        )
     }
 }
